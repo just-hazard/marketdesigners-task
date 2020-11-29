6d2e4b8f49875cd4bed3fde1822df072cc6e3a34
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-11-30 08:48:14 +0900

    feat : 특정시간 출근 인원 비즈니스 로직 작성중

---
+++ src/main/java/just/hazard/marketdesignerstask/MarketdesignersTaskApplication.java
@@ -0,0 +1,13 @@
+package just.hazard.marketdesignerstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdesignersTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketdesignersTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/just/hazard/marketdesignerstask/MarketdesignersTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package just.hazard.marketdesignerstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdesignersTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
